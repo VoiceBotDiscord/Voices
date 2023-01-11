e2bb844aaab8e3b4aa7f67a0ad598c270084fc36
ZetricsBOT <119356841+ZetricsBOT@users.noreply.github.com>
2023-01-11 19:48:40 +0300

    Voice
    fasf

--- src/strcut/base/Interaction.ts
+++
@@ -1,8 +0,0 @@
-import { ButtonRun, ModalRun } from '../../types/base/Interaction';
-
-export default class Interaction {
-    constructor(
-        public name: string,
-        public run: (ButtonRun | ModalRun)
-    ) {}
-}
